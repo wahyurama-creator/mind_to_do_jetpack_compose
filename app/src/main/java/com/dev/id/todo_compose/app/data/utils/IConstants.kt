58bd5b1f18db23e67629d5aa49db938aea538a05
wahyurama-creator <wahyuramas32@gmail.com>
2021-11-08 00:35:11 +0700

    Add constant variable for nav action

--- app/src/main/java/com/dev/id/todo_compose/app/data/utils/IConstants.kt
+++ app/src/main/java/com/dev/id/todo_compose/app/data/utils/IConstants.kt
@@ -1,6 +1,11 @@
 package com.dev.id.todo_compose.app.data.utils
 
 object IConstants {
+    // Database
     const val DATABASE_NAME = "task_todo_database"
     const val DATABASE_TABLE = "task_todo_table"
+
+    // Nav Action
+    const val LIST_SCREEN = "list/{action}"
+    const val TASK_SCREEN = "list/{taskId}"
 }
