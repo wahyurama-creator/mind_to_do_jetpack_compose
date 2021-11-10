1391294d8001db63998328b22e3b0e85f3dad0cf
wahyurama-creator <wahyuramas32@gmail.com>
2021-11-10 20:29:13 +0700

    Implement setup navigation

--- app/src/main/java/com/dev/id/todo_compose/MainActivity.kt
+++ app/src/main/java/com/dev/id/todo_compose/MainActivity.kt
@@ -3,21 +3,23 @@ package com.dev.id.todo_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.dev.id.todo_compose.ui.navigation.SetupNavigationController
 import com.dev.id.todo_compose.ui.theme.MindToDoToDoListReminderTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private lateinit var navController: NavHostController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MindToDoToDoListReminderTheme {
-
+                navController = rememberNavController()
+                SetupNavigationController(navController = navController)
             }
         }
     }
