899166854dbe7ea511b988824846c4b1285bd6e7
wahyurama-creator <wahyuramas32@gmail.com>
2021-11-10 20:26:57 +0700

    Add variable for args key

--- app/src/main/java/com/dev/id/todo_compose/app/data/utils/IConstants.kt
+++ app/src/main/java/com/dev/id/todo_compose/app/data/utils/IConstants.kt
@@ -8,4 +8,7 @@ object IConstants {
     // Nav Action
     const val LIST_SCREEN = "list/{action}"
     const val TASK_SCREEN = "list/{taskId}"
+    const val LIST_ARGUMENTS_KEY = "action"
+    const val TASK_ARGUMENTS_KEY = "taskId"
+
 }
