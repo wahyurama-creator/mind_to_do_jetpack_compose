cac3824ae3f5ef777420eaf8014e24f831070d22
wahyurama-creator <wahyuramas32@gmail.com>
2021-11-10 20:30:13 +0700

    Create setup navigation controller

---
+++ app/src/main/java/com/dev/id/todo_compose/ui/navigation/Navigation.kt
@@ -0,0 +1,24 @@
+package com.dev.id.todo_compose.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import com.dev.id.todo_compose.app.data.utils.IConstants.LIST_SCREEN
+import com.dev.id.todo_compose.ui.navigation.destionation.listComposable
+import com.dev.id.todo_compose.ui.navigation.destionation.taskComposable
+
+@Composable
+fun SetupNavigationController(navController: NavHostController) {
+    val screen = remember(navController) {
+        Screens(navController)
+    }
+    
+    NavHost(
+        navController = navController,
+        startDestination = LIST_SCREEN
+    ) {
+        listComposable(navigateToTaskScreen = screen.task)
+        taskComposable(navigateToListScreen = screen.list)
+    }
+}
