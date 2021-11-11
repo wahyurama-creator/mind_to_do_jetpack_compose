5cb68751da8ec7b8a4eef2625ca0037c71e64f53
wahyurama-creator <wahyuramas32@gmail.com>
2021-11-11 18:03:04 +0700

    Create list screen and add FAB view with icons default

---
+++ app/src/main/java/com/dev/id/todo_compose/ui/screen/list/ListScreen.kt
@@ -0,0 +1,43 @@
+package com.dev.id.todo_compose.ui.screen.list
+
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.dev.id.todo_compose.R
+
+@Composable
+fun ListScreen(navigateToTaskScreen: (Int) -> Unit) {
+    Scaffold(
+        content = {},
+        floatingActionButton = {
+            ListFAB(onFABClicked = navigateToTaskScreen)
+        }
+    )
+}
+
+@Composable
+fun ListFAB(
+    onFABClicked: (Int) -> Unit
+) {
+    FloatingActionButton(onClick = {
+        onFABClicked(-1)
+    }) {
+        Icon(
+            imageVector = Icons.Filled.Add,
+            contentDescription = stringResource(id = R.string.add_button),
+            tint = Color.White
+        )
+    }
+}
+
+@Composable
+@Preview
+private fun ListScreenPreview() {
+    ListScreen(navigateToTaskScreen = {})
+}
