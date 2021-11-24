9f6d366ed87e71f616c59bd9a0e1540be0be09cb
wahyurama-creator <wahyuramas32@gmail.com>
2021-11-24 21:38:13 +0700

    Add capitalize function

--- app/src/main/java/com/dev/id/todo_compose/ui/component/PriorityItem.kt
+++ app/src/main/java/com/dev/id/todo_compose/ui/component/PriorityItem.kt
@@ -8,25 +8,29 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.dev.id.todo_compose.app.data.model.Priority
 import com.dev.id.todo_compose.app.data.utils.IConstants.LARGE_PADDING
 import com.dev.id.todo_compose.app.data.utils.IConstants.PRIORITY_INDICATOR
 import com.dev.id.todo_compose.ui.theme.Typography
+import java.util.*
 
 @Composable
 fun PriorityItem(priority: Priority) {
     Row(
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceBetween,
-        modifier = Modifier.padding(8.dp)
     ) {
         Canvas(modifier = Modifier.size(PRIORITY_INDICATOR)) {
             drawCircle(color = priority.colorValue)
         }
         Spacer(modifier = Modifier.width(LARGE_PADDING))
         Text(
-            text = priority.name,
+            text = priority.name.lowercase(Locale.getDefault())
+                .replaceFirstChar {
+                    if (it.isLowerCase()) it.titlecase(
+                        Locale.getDefault()
+                    ) else it.toString()
+                },
             style = Typography.subtitle1,
             color = MaterialTheme.colors.onSurface
         )
