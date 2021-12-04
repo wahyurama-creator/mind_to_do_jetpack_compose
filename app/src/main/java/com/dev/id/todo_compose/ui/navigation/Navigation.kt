f83eda7c6d4a594958a8e953b79413baf5fead3d
wahyurama-creator <wahyuramas32@gmail.com>
2021-12-05 00:44:04 +0700

    add shared view model for parameter

--- app/src/main/java/com/dev/id/todo_compose/ui/navigation/Navigation.kt
+++ app/src/main/java/com/dev/id/todo_compose/ui/navigation/Navigation.kt
@@ -2,23 +2,32 @@ package com.dev.id.todo_compose.ui.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import com.dev.id.todo_compose.app.data.utils.IConstants.LIST_SCREEN
 import com.dev.id.todo_compose.ui.navigation.destination.listComposable
 import com.dev.id.todo_compose.ui.navigation.destination.taskComposable
+import com.dev.id.todo_compose.ui.viewmodel.SharedViewModel
 
+@ExperimentalComposeUiApi
 @Composable
-fun SetupNavigationController(navController: NavHostController) {
+fun SetupNavigationController(
+    navController: NavHostController,
+    viewModel: SharedViewModel
+) {
     val screen = remember(navController) {
         Screens(navController)
     }
-    
+
     NavHost(
         navController = navController,
         startDestination = LIST_SCREEN
     ) {
-        listComposable(navigateToTaskScreen = screen.task)
+        listComposable(
+            navigateToTaskScreen = screen.task,
+            viewModel = viewModel
+        )
         taskComposable(navigateToListScreen = screen.list)
     }
 }
