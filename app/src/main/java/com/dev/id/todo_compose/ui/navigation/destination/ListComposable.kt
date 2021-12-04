7f668c40e1336e3082bf4781e41a4adbf0d7b3b4
wahyurama-creator <wahyuramas32@gmail.com>
2021-12-05 00:46:05 +0700

    add shared view model for parameter

--- app/src/main/java/com/dev/id/todo_compose/ui/navigation/destination/ListComposable.kt
+++ app/src/main/java/com/dev/id/todo_compose/ui/navigation/destination/ListComposable.kt
@@ -1,5 +1,6 @@
 package com.dev.id.todo_compose.ui.navigation.destination
 
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
 import androidx.navigation.compose.composable
@@ -7,14 +8,22 @@ import androidx.navigation.navArgument
 import com.dev.id.todo_compose.app.data.utils.IConstants.LIST_ARGUMENTS_KEY
 import com.dev.id.todo_compose.app.data.utils.IConstants.LIST_SCREEN
 import com.dev.id.todo_compose.ui.screen.list.ListScreen
+import com.dev.id.todo_compose.ui.viewmodel.SharedViewModel
 
-fun NavGraphBuilder.listComposable(navigateToTaskScreen: (taskId: Int) -> Unit) {
+@ExperimentalComposeUiApi
+fun NavGraphBuilder.listComposable(
+    navigateToTaskScreen: (taskId: Int) -> Unit,
+    viewModel: SharedViewModel
+) {
     composable(
         route = LIST_SCREEN,
         arguments = listOf(navArgument(LIST_ARGUMENTS_KEY) {
             type = NavType.StringType
         })
     ) {
-        ListScreen(navigateToTaskScreen = navigateToTaskScreen)
+        ListScreen(
+            navigateToTaskScreen = navigateToTaskScreen,
+            viewModel = viewModel
+        )
     }
 }
