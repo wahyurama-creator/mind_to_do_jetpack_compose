60e760d792cc5d0640bbd0ed7f30c47bbb6389a8
wahyurama-creator <wahyuramas32@gmail.com>
2021-12-06 20:25:55 +0700

    add annotation ExperimentalMaterialApi and move to top

--- app/src/main/java/com/dev/id/todo_compose/MainActivity.kt
+++ app/src/main/java/com/dev/id/todo_compose/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
@@ -12,13 +13,14 @@ import com.dev.id.todo_compose.ui.theme.MindToDoToDoListReminderTheme
 import com.dev.id.todo_compose.ui.viewmodel.SharedViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
+@ExperimentalMaterialApi
+@ExperimentalComposeUiApi
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     private lateinit var navController: NavHostController
     private val sharedViewModel: SharedViewModel by viewModels()
 
-    @ExperimentalComposeUiApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
