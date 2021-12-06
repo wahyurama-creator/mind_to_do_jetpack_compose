bb6d22c75e3b3ac5748480d086265ec5dcef6485
wahyurama-creator <wahyuramas32@gmail.com>
2021-12-06 20:24:11 +0700

    ListScreen
    - add LaunchedEffect
    - add trackLogs
    - add allTask variable
    - add parameter on ListContent

--- app/src/main/java/com/dev/id/todo_compose/ui/screen/list/ListScreen.kt
+++ app/src/main/java/com/dev/id/todo_compose/ui/screen/list/ListScreen.kt
@@ -1,28 +1,39 @@
 package com.dev.id.todo_compose.ui.screen.list
 
-import androidx.compose.material.FloatingActionButton
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.dev.id.todo_compose.R
+import com.dev.id.todo_compose.app.data.utils.IConstants
+import com.dev.id.todo_compose.app.data.utils.IConstants.SCREEN_NAME
 import com.dev.id.todo_compose.app.data.utils.SearchAppBarState
 import com.dev.id.todo_compose.ui.theme.fabBackgroundColor
 import com.dev.id.todo_compose.ui.viewmodel.SharedViewModel
 
+@ExperimentalMaterialApi
 @ExperimentalComposeUiApi
 @Composable
 fun ListScreen(
     navigateToTaskScreen: (taskId: Int) -> Unit,
     viewModel: SharedViewModel
 ) {
+    LaunchedEffect(key1 = true) {
+        IConstants.trackLogs(
+            SCREEN_NAME,
+            "List screen - LaunchEffect triggered!"
+        )
+        viewModel.getAllToDoTask()
+    }
+
+    val allToDoTask by viewModel.allToDoTask.collectAsState()
     val searchAppBarState: SearchAppBarState by viewModel.searchAppBarState
     val searchTextState: String by viewModel.searchTextState
 
@@ -35,7 +46,10 @@ fun ListScreen(
             )
         },
         content = {
-            ListContent()
+            ListContent(
+                tasks = allToDoTask,
+                navigateToTaskScreen = navigateToTaskScreen
+            )
         },
         floatingActionButton = {
             ListFAB(onFABClicked = navigateToTaskScreen)
